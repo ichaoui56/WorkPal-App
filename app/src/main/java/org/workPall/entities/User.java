caf6b03a267b327b92ef7a2d2133f84db47bc28a
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-09-10 11:14:42 +0100

    create a user entity

---
+++ app/src/main/java/org/workPall/entities/User.java
@@ -0,0 +1,118 @@
+package org.workPall.entities;
+
+import org.workPall.enums.Role;
+import org.workPall.orm.annotations.Column;
+import org.workPall.orm.annotations.Table;
+
+@Table(name= "users")
+public class User {
+
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="role")
+    private Role role;
+
+    public User( String firstName, String lastName, String email, String phoneNumber, String address, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String toString() {
+        return "User [id=" + id + ", " +
+                "firstName=" + firstName + ", " +
+                "lastName=" + lastName + ", " +
+                "email=" + email + ", " +
+                "phoneNumber=" + phoneNumber + ", " +
+                "address=" + address + ", " +
+                "password=" + password + ", " +
+                "role=" + role + "]";
+    }
+}

21ae437f8c9822171bb5cc1b0b2c92ae79a8a665
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-09-10 11:19:52 +0100

    Create UserServiceInter.java

---
+++ app/src/main/java/org/workPall/services/interfaces/UserServiceInter.java
@@ -0,0 +1,14 @@
+package org.workPall.services.interfaces;
+
+import org.workPall.entities.User;
+import java.util.List;
+
+public interface UserServiceInter {
+    List<User> getAllUsers();
+    void logOut();
+    User getUserData();
+    User createUser(User user);
+    Boolean login(String email, String password);
+    void updateUser(User user);
+    void deleteUser(int id);
+}
